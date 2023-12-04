cfcd93d3dc472ce8bb0d339b4f8cf016b1b377fe
7057570470 <varsha.kadge@atg.auto>
2023-12-04 21:50:37 +0530

    Created Project

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class Welcome extends Component {
+    render(){
+        return <h1>Welcome Class</h1>
+    }
+
+}
+
+export default Welcome
